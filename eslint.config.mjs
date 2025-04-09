bfeabeb2aab45c0b38ff447c8425d81c43acc987
Nikita12-3 <nekit0313@yandex.ru>
2025-04-09 09:13:14 +0400

    linterComm2

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+
+export default defineConfig([
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Linter1.
+          Edit <code>src/App.js</code> Linter2.
         </p>
         <a
           className="App-link"

dd2074186471ebda978d60bf5c732481924e3161
Nikita12-3 <nekit0313@yandex.ru>
2025-04-09 09:07:32 +0400

    linterComm1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Linter.
+          Edit <code>src/App.js</code> Linter1.
         </p>
         <a
           className="App-link"

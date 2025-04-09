e150e362c3f60a2ec3882202039ab0b4962967c9
Nikita12-3 <nekit0313@yandex.ru>
2025-04-09 09:23:33 +0400

    linterCommRun

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    'max-len': ['error', { 'code': 80 }],
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        'indent': ['error', 2],
+      },
+    },
+    {
+      files: ['**/*.{js,mjs,cjs,jsx}'],
+      rules: {
+        'no-undef': 'off', // Пример отключения правила для определенных файлов
+      },
+    },
+  ],
+};
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Linter2.
+          Edit <code>src/App.js</code> LinterRuns.
         </p>
         <a
           className="App-link"
